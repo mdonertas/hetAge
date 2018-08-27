e034757943a34bb8bda2c28390c0aebab2cad595
melike <melike@ebi.ac.uk>
2018-08-27 14:19:52 +0100

    core functions + website

---
+++ R/heterogeneity.R
@@ -0,0 +1,126 @@
+#' Calculate Age-related Expression and Heterogeneity Changes for Gene
+#'
+#' @param exprs a numeric vector with the expression values, ordered the same as age vector
+#' @param age a numeric vector, where the names correspond to samples (the same as colnames of the given matrix).
+#' @param modex expression change calculation method. 'linear' or 'loess', defaults to 'linear'
+#' @param het_change_met heterogeneity change calculation method. 'LR', for 'linear regression', or and correlation method accepted by cor.test() function.
+#'
+#' @return a list with i) summary results with the effect size and p value for expression level and heterogeneity changes, ii) residuals from expr~age model, which corresponds to unexplained variance, iii) expression values passed into the function.
+#' @export
+#'
+calc.het.feat <- function(exprs, age, modex = c('linear','loess'),
+                          het_change_met) {
+    if ( modex == 'linear') {
+        lmx <- lm(exprs ~ age)
+        expr_change <- unname(lmx$coef[2])
+        expr_change.p <- unname(summary(lmx)$coef[2, 4])
+    } else if ( modex == 'loess') {
+        lmx <- loess(exprs ~ age)
+        expr_change <- NA
+        expr_change.p <- NA
+    }
+    resids <- lmx$resid
+    if ( het_change_met == "LR") {
+        lmxx <- lm(abs(resids) ~ age)
+        het_change <- unname(lmxx$coefficients[2])
+        het_change.p <- unname((summary(lmxx)$coef)[2,4])
+    } else {
+        co_het <- cor.test(abs(resids), age, method = het_change_met)
+        het_change <- unname(co_het$est)
+        het_change.p <- unname(co_het$p.val)
+    }
+    res <- list(summary = c(level_change = expr_change,
+                            level_change.p = expr_change.p,
+                            heterogeneity_change = het_change,
+                            heterogeneity_change.p = het_change.p))
+    res$residuals <- resids
+    res$expression <- exprs
+    return(res)
+}
+
+#' Calculate Age-related Expression and Heterogeneity Changes
+#'
+#' @param exprmat a numeric matrix with the expression values, where columns are the samples and rows are probesets, transcripts, or genes.
+#' @param age a numeric vector, where the names correspond to samples (the same as colnames of the given matrix).
+#' @param age_in type of the age vector, allowed values are days or years. defaults to 'days'
+#' @param age_to final format of age vector. allowed values are 'years', 'days', 'pw-N', and 'lg-N', where N is any number. 'pw' means power. e.g. pw-0.5 means sqrt(age), and 'lg' means log, e.g. lg-2 means log2(age).defaults to 'pw-0.25'
+#' @param batch_corr batch correction strategy. available values are i) NC: No Correction, ii) QN: Quantile Normalization, iii) LR: Linear regression (requires covariates), iv) SVA: surrogate variable analysis, v) LR+QN: Linear regression followed by quantile normalization, and vi) SVA+QN: SVA followed by quantile normalization,
+#' @param modex expression change calculation method. 'linear' or 'loess', defaults to 'linear'
+#' @param tr_log2 logical to set log2 transforming expression matrix. defaults to TRUE.
+#' @param sc_features logical to set whether to scale features. defaults to TRUE.
+#' @param covariates a list of covarietes where each element is a vector with sampleIDs as names
+#' @param het_change_met heterogeneity change calculation method. 'LR', for 'linear regression', or and correlation method accepted by cor.test() function.
+#' @param padj_met method for multiple test correction. value is passed to 'p.adjust' function. defaults to 'fdr'.
+#'
+#' @return a list object with summary results including expression level and heterogeneity changes, input values, intermediate values, and heterogeneity matrix.
+#'
+#' @importFrom tibble as.tibble
+#' @importFrom dplyr select
+#' @importFrom dplyr everything
+#'
+#' @export
+#'
+calc.het <- function(exprmat, age, age_in = c('days','years'),
+                     age_to = c('pw-0.25','years'),
+                     batch_corr = c("NC", "QN", "LR", "SVA", "LR+QN", "SVA+QN"),
+                     modex = c("linear", "loess"),
+                     tr_log2 = c(T, F), sc_features = c(T,F),
+                     covariates = NA,
+                     het_change_met = c("spearman","pearson","LR"),
+                     padj_met = c("fdr","BY")) {
+    retval <- list()
+    snms <- intersect(colnames(exprmat), names(age))
+    retval$sampleID <- snms
+    exprmat <- exprmat[, snms]
+    retval$input_expr <- exprmat
+    age <- age[snms]
+    if ( !all(is.na(covariates)) ) { covariates = lapply(covariates,
+                                                    function(cov) cov[snms] )}
+    retval$input_age <- age
+    age <- transform_age(age, type = age_in, to = age_to)
+    retval$usedAge <- age
+    exprmat <- exprmat[complete.cases(exprmat),]
+    if (tr_log2 == T) {
+        exprmat <- trans_log2(exprmat)
+    }
+    if (batch_corr == "NC") {
+        exprmat <- exprmat
+    } else if (batch_corr == "QN") {
+        exprmat <- cfr_quantileNorm(exprmat)
+    } else if (batch_corr == "LR") {
+        LR_res <- cfr_linReg(exprmat, covariates)
+        exprmat <- LR_res$correctedExp
+        retval$LR_res <- LR_res
+    } else if (batch_corr == "LR+QN") {
+        LR_res <- cfr_linReg(exprmat, covariates)
+        exprmat <- LR_res$correctedExp
+        exprmat <- cfr_quantileNorm(exprmat)
+        retval$LR_res <- LR_res
+    } else if (batch_corr == "SVA") {
+        sva_res <- cfr_sva(exprmat, age, covariates)
+        exprmat <- sva_res$correctedExp
+        retval$sva_res <- sva_res
+    } else if (batch_corr == "SVA+QN") {
+        sva_res <- cfr_sva(exprmat, age, covariates)
+        exprmat <- sva_res$correctedExp
+        exprmat <- cfr_quantileNorm(exprmat)
+        retval$sva_res <- sva_res
+    }
+    exprmat <- feature_scale(exprmat)
+    exprmat <- exprmat[complete.cases(exprmat),]
+    resx <- apply(exprmat, 1, function(expx) {
+        calc.het.feat(expx, age, modex = modex,
+                      het_change_met = het_change_met)
+    })
+    feature_het <- tibble::as.tibble(t(sapply(resx, function(x) x$summary)))
+    feature_het$level_change.p.adj <- p.adjust(feature_het$level_change.p,
+                                               method = padj_met)
+    feature_het$heterogeneity_change.p.adj <- p.adjust(feature_het$heterogeneity_change.p,
+                                              method = padj_met)
+    feature_het$feature <- rownames(exprmat)
+    feature_het <- dplyr::select(feature_het, 'feature', dplyr::everything())
+    retval$residMat <- t(sapply(resx,function(x)x$resid))
+    retval$usedMat <- t(sapply(resx,function(x)x$expr))
+    retval$feature_result <- feature_het
+    return(retval)
+}
---
+++ R/preprocessing.R
@@ -0,0 +1,203 @@
+#' Log2 Transformation
+#'
+#' @param mat a numeric matrix with the expression values, where columns are the samples and rows are probesets, transcripts, or genes.
+#'
+#' @return log2 transformed matrix
+#'
+#' @export
+#'
+#' @examples
+#' myexp <- sapply(1:10, function(i) rnbinom(10000, mu = 4, size = 1))
+#' myexp_l2 <- trans_log2(myexp)
+#' par(mfrow = c(1,2))
+#' hist(myexp)
+#' hist(myexp_l2)
+#'
+trans_log2 <- function(mat){
+    return(log2(mat + 1))
+}
+
+#' Quantile Normalize Expression Values
+#'
+#' A function to apply quantile normalization. It is build on preprocessCore::normalize.quantiles function, but keeps the dimnames as is.
+#'
+#' @param mat a numeric matrix with the expression values, where columns are the samples and rows are probesets, transcripts, or genes.
+#'
+#' @return quantile normalized expression values as a matrix
+#'
+#' @importFrom preprocessCore normalize.quantiles
+#'
+#' @export
+#' @examples
+#' myexp <- sapply( 1:10, function(i){
+#' rnorm(n = 10000, mean = sample(1:3, 1), sd = sample(c(1, 3), 1))
+#' })
+#' myexp_qn <- cfr_quantileNorm(myexp)
+#' par(mfrow = c(1,2))
+#' boxplot(myexp, main = 'Before QN')
+#' boxplot(myexp_qn, main = 'After QN')
+#'
+
+cfr_quantileNorm <- function(mat) {
+    res <- preprocessCore::normalize.quantiles(mat)
+    colnames(res) <- colnames(mat)
+    rownames(res) <- rownames(mat)
+    return(res)
+}
+
+#' Construct model matrix
+#'
+#' Using a list of covarietes, constructs a model matrix to be used in a model
+#'
+#' @param covs a list of covarietes
+#'
+#' @return a dataframe that can be used as a design matrix in a model
+#'
+#' @export
+#' @examples
+#' mycovs <- list(batch = sample(c(0, 1), 6, replace = TRUE),
+#' array = as.factor(sample(c('a', 'b', 'c'), 6, replace = TRUE)),
+#' day = sample(c(0, 1, 3), 6, replace = TRUE))
+#' des_mat <- const.mod(mycovs)
+#' print(mycovs)
+#' print(des_mat)
+#'
+const.mod <- function(covs) {
+    res <- lapply(covs, function(nm) {
+        model.matrix(~0 + nm)
+    })
+    res2 <- matrix(ncol = 0, nrow = length(covs[[1]]))
+    for (i in names(res)) {
+        if (ncol(res[[i]]) > 1) {
+            colnames(res[[i]]) <- paste(i, levels(covs[[i]]), sep = ".")
+        } else {
+            colnames(res[[i]]) <- i
+        }
+        res2 <- cbind(res2, res[[i]])
+    }
+    return(res2)
+}
+
+#' Removing Confounding Factor Effects using Linear Regression
+#'
+#' @param mat a numeric matrix with the expression values, where columns are the samples and rows are probesets, transcripts, or genes.
+#' @param cov a list with covariates to regress out from the expression matrix
+#'
+#' @return a list with three elements, i) corrected expression matrix, ii) coefficients for the covariates and iii) p values for each covariate
+#'
+#' @export
+#'
+
+cfr_linReg <- function(mat, cov){
+    cov <- const.mod(cov)
+    temp_mat = apply(mat, 1, function(x){
+        lmx = lm(x ~ cov)
+    })
+    corr_mat = t(sapply(temp_mat, function(x) x$resid ))
+    colnames(corr_mat) <- colnames(mat)
+    rownames(corr_mat) <- rownames(mat)
+    cov_coef <- t(sapply(temp_mat, function(x) x$coeff ))
+    colnames(cov_coef) <- gsub('covsx', '', colnames(cov_coef))
+    cov_p <- t(sapply(temp_mat, function(x) (summary(x)$coef)[,4] ))
+    colnames(cov_p) <- gsub('covsx', '', colnames(cov_p))
+    res <- list(correctedExp = corr_mat,
+                cov_coef = cov_coef,
+                cov_p = cov_p)
+    return(res)
+}
+
+#' Removing Confounding Factor Effects using SVA
+#'
+#' @param mat a numeric matrix with the expression values, where columns are the samples and rows are probesets, transcripts, or genes.
+#' @param age a numeric vector, where the names correspond to samples (the same as colnames of the given matrix).
+#' @param cov covariates to analyse SVs.
+#'
+#' @return a list with i) a numeric matrix with the expression values, ii) SVs for each sample, and iii) correlation between SVs and covariates
+#'
+#' @importFrom sva sva
+#' @importFrom reshape2 melt
+#'
+#' @export
+
+
+cfr_sva <- function(mat, age, cov = NA) {
+    samples <- unique(intersect(colnames(mat), names(age)))
+    mat <- mat[, samples]
+    age <- age[samples]
+    mod <- model.matrix(~age, data = as.data.frame(colnames(mat)))
+    mod0 <- model.matrix(~1, data = as.data.frame(colnames(mat)))
+    svobj <- sva::sva(mat, mod, mod0)
+    if (!all(is.na(cov))) {
+        sv_cov_cors <- apply(svobj$sv, 2, function(x) {
+            cx <- const.mod(cov)
+            lmx <- lm(x ~ cx)
+            estx <- lmx$coef[-1]
+            names(estx) <- substr(names(estx),3,nchar(names(estx)))
+            px <- summary(lmx)$coef[-1, 4]
+            names(px) <- substr(names(px),3,nchar(names(px)))
+            resultx <- data.frame(estimate = estx[colnames(cx)],
+                       p = px[colnames(cx)])
+            resultx$p.adj <- p.adjust(resultx$p,'fdr')
+            resultx$cov <- colnames(cx)
+            resultx
+        })
+        sv_cov_cors <- reshape2::melt(sv_cov_cors,id.vars = c('cov','estimate',
+                                                              'p', 'p.adj'))
+        colnames(sv_cov_cors)[5] = 'SV'
+    } else {
+        sv_cov_cors <- NULL
+    }
+    corr.mat <- t(apply(mat, 1, function(genexp) lm(genexp ~ svobj$sv)$resid))
+    SVs <- svobj$sv
+    colnames(SVs) = paste('SV', 1:ncol(SVs), sep = '_')
+    rownames(SVs) = samples
+    res <- list(correctedExp = corr.mat, SVs = SVs, SV_cov_corr = sv_cov_cors)
+    return(res)
+}
+
+#' Scale Expression Values for Features (probsets, transcripts, genes)
+#'
+#' @param mat a numeric matrix with the expression values, where columns are the samples and rows are probesets, transcripts, or genes.
+#'
+#' @return scaled matrix
+#' @export
+#'
+feature_scale <- function(mat) {
+    res <- t(apply(mat, 1, scale))
+    colnames(res) <- colnames(mat)
+    rownames(res) <- rownames(mat)
+    return(res)
+}
+
+#' Age Transforming Function
+#'
+#' @param age a numeric vector with ages
+#' @param type type of the age vector, allowed values are days or years. defaults to 'days'
+#' @param to final format of age vector. allowed values are 'years', 'days', 'pw-N', and 'lg-N', where N is any number. 'pw' means power. e.g. pw-0.5 means sqrt(age), and 'lg' means log, e.g. lg-2 means log2(age)
+#'
+#' @return a vector with ages in the desired scale
+#' @export
+#'
+#' @examples
+#' agevec <- seq(1:20)
+#' transform_age(agevec, type = 'years', to = 'days')
+#' transform_age(agevec, type = 'years', to = 'pw-0.25')
+#' transform_age(agevec, type = 'years', to = 'lg-10')
+#'
+transform_age <- function(age, type = c('days', 'years'),
+                          to = c('years', 'days', 'pw-N', 'lg-N')) {
+    if (type == 'years') {
+        age <- age * 365
+    }
+    if (to == 'days') {
+        return(age)
+    } else if (to == 'years') {
+        return(age/365)
+    } else if (substr(to, 1, 2) == 'pw') {
+        trval <- as.numeric(strsplit(to, '-')[[1]][2])
+        return(age^trval)
+    } else if (substr(to, 1, 2) == 'lg') {
+        trval <- as.numeric(strsplit(to, '-')[[1]][2])
+        return(log(age,base = trval))
+    }
+}
